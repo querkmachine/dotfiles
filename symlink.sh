9f0450c3c1deedcf75aca58955ac909836ed705b
Kimberly Grey <kimandthemachine@gmail.com>
2017-04-07 20:28:33 +0100

    Add macOS preferences file

--- symlink.sh
+++ symlink.sh
@@ -3,12 +3,14 @@
 rm ~/.bashrc
 rm ~/.gitconfig
 rm ~/.gitignore_global 
-rm ~/.zshrc
 rm ~/.profile
+rm ~/.zshrc
 
 ln -s ~/dotfiles/bashrc ~/.bashrc 
 ln -s ~/dotfiles/gitconfig ~/.gitconfig 
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global 
-ln -s ~/dotfiles/kim.zsh-theme ~/.oh-my-zsh/themes/kim.zsh-theme 
-ln -s ~/dotfiles/zshrc ~/.zshrc 
 ln -s ~/dotfiles/profile ~/.profile 
+ln -s ~/dotfiles/zshrc ~/.zshrc 
+ln -s ~/dotfiles/kim.zsh-theme ~/.oh-my-zsh/themes/kim.zsh-theme 
+
+bash ./macos
