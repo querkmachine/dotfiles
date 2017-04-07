ded7dab70409c4b3b84d3a39b90240985177eaed
Kimberly Grey <kimandthemachine@gmail.com>
2017-04-07 16:08:11 +0100

    Initial commit

---
+++ bashrc
@@ -0,0 +1,2 @@
+alias yolo='git add -A . && git commit -m "YOLO"'
+alias swag='git push origin'
---
+++ symlink.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s ~/dotfiles/bashrc ~/.bashrc 
+ln -s ~/dotfiles/gitconfig ~/.gitconfig 
+ln -s ~/dotfiles/gitignore_global ~/.gitignore_global 
+ln -s ~/dotfiles/kim.zsh-theme ~/.oh-my-zsh/themes/kim.zsh-theme 
+ln -s ~/dotfiles/zshrc ~/.zshrc 
