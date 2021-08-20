4adcd147b09ce8903baa8a7f07036211b00d64e5
Kimberly Grey <kimandthemachine@gmail.com>
2021-08-20 10:23:21 +0100

    Delete Atom config files.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ rm ~/.gitignore_global
 rm ~/.p10k.zsh
 rm ~/.profile
 rm ~/.zshrc
-rm ~/.atom/config.cson
 
 # Symlink files from this repo to those locations
 ln -s ~/dotfiles/.bashrc ~/.bashrc
@@ -16,10 +15,6 @@ ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/atom-config.cson ~/.atom/config.cson
-
-# Install Atom packages
-apm install --packages-file ~/dotfiles/atom-packages.list
 
 # Run macOS configuration script
 source ~/dotfiles/macos-config.sh
