45db15a284932a69fcbcf56cb4b9158f059b3d4e
Kimberly Grey <hi+github@berly.kim>
2022-07-12 22:36:54 +0100

    Update macOS config

--- macos-config.sh
+++ macos-config.sh
@@ -19,10 +19,6 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Accessibility: Use scroll gesture with the Ctrl (^) modifier key to zoom
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
-
-# Accessibility: Follow the keyboard focus while zoomed in
-defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
 # Activity Monitor: Show the main window when launching Activity Monitor
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
@@ -30,14 +26,27 @@ defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 # Activity Monitor: Visualize CPU usage in the Activity Monitor Dock icon
 defaults write com.apple.ActivityMonitor IconType -int 5
 
+# Activity Monitor: Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Activity Monitor: Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
 # AirDrop: Enable AirDrop over Ethernet and on unsupported Macs running Lion
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# App Store: Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
 
 # App Store: Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # App Store: Download newly available updates in background
 defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# App Store: Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
 
 # App Store: Turn on app auto-update
 defaults write com.apple.commerce AutoUpdate -bool true
@@ -59,6 +68,15 @@ defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
 # Dock: Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Dock: Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Dock: Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Dock: Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 
 # Finder: Set Desktop as the default location for new Finder windows
 # For other paths, use `PfLo` and `file:///full/path/here/`
@@ -90,8 +108,13 @@ defaults write NSGlobalDomain com.apple.springing.delay -float 0
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# iTerm 2: Don’t display the annoying prompt when quitting iTerm
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+# Hot Corner: Top right opens Notification Center
+defaults write com.apple.dock wvous-tr-corner -int 12
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Hot Corner: Bottom right shows Desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 # L10N: Set language and units
 defaults write NSGlobalDomain AppleLanguages -array "en"
@@ -122,6 +145,15 @@ defaults write com.apple.Safari HomePage -string "about:blank"
 # Safari: Prevent Safari from opening ‘safe’ files automatically after downloading
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
+# Safari: Disable auto-correct
+defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+
+# Safari: Disable AutoFill
+defaults write com.apple.Safari AutoFillFromAddressBook -bool false
+defaults write com.apple.Safari AutoFillPasswords -bool false
+defaults write com.apple.Safari AutoFillCreditCardData -bool false
+defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
+
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
@@ -129,9 +161,6 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Trackpad: Disable the Launchpad gesture (pinch with thumb and three fingers)
 defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
-
-# Tweetbot: Bypass the annoyingly slow t.co URL shortener
-defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
 
 # UI: Only show scrollbars when scrolling
 defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
@@ -143,7 +172,7 @@ defaults write com.apple.screencapture disable-shadow -bool true
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
 # UI: Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture type -string "png"
+defaults write com.apple.screencapture type -string "jpg"
 
 # UI: Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -161,11 +190,8 @@ for app in "Activity Monitor" \
 	"cfprefsd" \
 	"Dock" \
 	"Finder" \
-	"Photos" \
 	"Safari" \
-	"SystemUIServer" \
-	"Terminal" \
-	"Tweetbot"; do
+	"SystemUIServer"; do
 	killall "${app}" &> /dev/null
 done
 
