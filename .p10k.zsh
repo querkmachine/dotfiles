ee36f0a9dd5a68187abc34c511051509c5a32099
beeps <hi+github@berly.kim>
2025-10-01 08:10:46 +0100

    Update terminal and Sublime theme configs

--- .p10k.zsh
+++ .p10k.zsh
@@ -359,7 +359,7 @@
 
   #####################################[ vcs: git status ]######################################
   # Version control background colors.
-  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=2
+  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=7
   typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=3
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
   typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
